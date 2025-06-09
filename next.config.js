ff967d48e54b93322d36ffd8fef7164afde8b586
ARAF-005 <mostafizur.araf@outlook.com>
2025-06-09 13:55:57 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const { i18n } = require('./next-i18next.config')
 module.exports = {
   i18n,
   reactStrictMode: true,
-  // Required by Next i18n with API routes, otherwise API routes 404 when fetching without trailing slash
-  trailingSlash: true
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // 👈 This disables the need for 'sharp'
+  },
 }
